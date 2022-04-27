9aa8d83b2ea1a15b7e41f55134ef232e1b140c15
Seth B <tehsethinator@gmail.com>
2022-04-26 21:13:20 -0500

    set up db connection and running queries on RDS instance

--- connect_RDS.py
+++ connect_RDS.py
@@ -1,21 +1,36 @@
 import pymysql
 
-db = pymysql.connect(host='database-1.ck62t6bhkvok.us-east-2.rds.amazonaws.com', user='admin', password='Pass123!', database='test')
+connection = pymysql.connect(host='database-1.ck62t6bhkvok.us-east-2.rds.amazonaws.com', user='admin', password='Pass123!', database='test')
 
-cursor = db.cursor()
-print(cursor)
+with connection:
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO test (`employee`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+    #why need to commit?
+    connection.commit()
 
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM test"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
+
+
+# cursor = db.cursor()
+# print(cursor)
+
+#CREATE DATABASE
 #sql = '''create database test'''
 #cursor.execute(sql)
 
 # sql = 'CREATE TABLE test(employee varchar(32), password varchar(32))'
 # cursor.execute(sql)
 
-sql = "INSERT INTO test (`employee`, `password`) VALUES (%s, %s)"
-cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
-
-print(cursor.execute("select * from test"))
+# sql = "INSERT INTO test (`employee`, `password`) VALUES (%s, %s)"
+# cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+#
+#
+# print(cursor.execute("select * from test"))
 
 
 
