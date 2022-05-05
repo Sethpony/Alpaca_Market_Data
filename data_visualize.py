a71b9134be505d6cda16178844b4dda180488347
Seth B <tehsethinator@gmail.com>
2022-05-04 20:21:24 -0500

    using config file to run get_bars api call

--- data_visualize.py
+++ data_visualize.py
@@ -11,9 +11,11 @@ from util import load_config
 # run the program with parser
 # python data_visualize.py --config ./config_files/config_job_run.json
 
-#first create argument parser object, then enact on that object
-parser = argparse.ArgumentParser(description="retrieves arguments needed for using get_bars call from Alpaca Market Data API")
-parser.add_argument('--config', action='store', help='add input parameter for location of config file to run API call job')
+# first create argument parser object, then enact on that object
+parser = argparse.ArgumentParser(
+    description="retrieves arguments needed for using get_bars call from Alpaca Market Data API")
+parser.add_argument('--config', action='store',
+                    help='add input parameter for location of config file to run API call job')
 
 args = parser.parse_args()
 print(f'this the arguments that have been passed {args}')
@@ -21,17 +23,11 @@ print(f'this is the arguments that have been passed: {args.config}')
 
 print(f'{load_config(args.config)}')
 
-
 conf = load_config(args.config)
 print(f'print conf:{conf}')
 keys = conf
 
-if('jobs' in keys):
-    for job in conf['jobs']:
-        print('run')
-        print(job)
-
-
+#### start of API Calls ####
 base_url = "wss://stream.data.alpaca.markets/v2/iex"
 
 # set up auth credentials (key and secret key)
@@ -42,13 +38,28 @@ APCA_API_SECRET_KEY = config.APCA_API_SECRET_KEY
 api = tradeapi.REST(key_id=APCA_API_KEY_ID, secret_key=APCA_API_SECRET_KEY,
                     base_url=base_url, api_version='v2')
 
+if ('jobs' in keys):
+    for job in conf['jobs']:
+        print('printing the values tied to the jobs key')
+        print(job)
+        symbol = job['symbol']
+        time_frame = job['time_frame']
+        start_date = job['start_date']
+        end_date = job['end_date']
+        print(symbol)
+        spy_bars = api.get_bars(symbol, time_frame, start_date, end_date)
+        print(spy_bars)
+        # spy_bars = api.get_bars(str(job['symbol']), str(job['time_frame']), str(job['start_date']), str(job['end_date']))
+    # print(spy_bars)
+    # spy_bars = api.get_bars(symbol, timeframe, start, end)
+
 symbol = "SPY"
 timeframe = "1Day"
 start = "2021-01-01"
 end = "2021-01-30"
 # Retrieve daily bars for SPY in a dataframe and printing the first 5 rows
-spy_bars = api.get_bars(symbol, timeframe, start, end)
-# print(spy_bars)
+#spy_bars = api.get_bars(symbol, timeframe, start, end)
+#print(spy_bars)
 # spy_bars = api.get_trades('AAPL', '2020-01-25', '2020-01-30',5)
 # print(spy_bars)
 
